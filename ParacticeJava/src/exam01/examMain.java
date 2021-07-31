90b80d9344fcbaf0354b10726175caa7114b9672
DaBinSeo <87168961+DaBinSeo@users.noreply.github.com>
2021-07-31 16:25:00 +0900

    CreateExample Project

---
+++ ParacticeJava/src/exam01/examMain.java
@@ -0,0 +1,15 @@
+package exam01;
+
+public class examMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=2, b=9;
+		int sum=0;
+		
+		sum=a+b;
+		System.out.println(sum);
+	}
+
+}
